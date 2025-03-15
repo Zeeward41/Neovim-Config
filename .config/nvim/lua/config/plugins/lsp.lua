4d3546a4c25d0efac8ddc1139b85be399306e08d
Zeeward41 <zeeward41@gmail.com>
2025-03-15 17:00:21 +0100

    add: lsp go + add: function look for root

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -15,6 +15,7 @@ return {
             require("lspconfig").lua_ls.setup { capabilities = capabilities }
             require("lspconfig").pyright.setup {
                 capabilities = capabilities,
+                root_dir = require("lspconfig.util").root_pattern("requirements.txt", "setup.py", ".git"),
                 filetypes = { 'py' },
                 settings = {
                     python = {
@@ -24,8 +25,29 @@ return {
                     }
                 }
             }
-            require("lspconfig").ts_ls.setup { capabilities = capabilities }
-            require("lspconfig").terraformls.setup { capabilities = capabilities }
+            require("lspconfig").ts_ls.setup {
+                capabilities = capabilities,
+                root_dir = require("lspconfig.util").root_pattern("tsconfig.json", "eslint.config.js", "package.json", ".git"),
+            }
+            require("lspconfig").terraformls.setup {
+                capabilities = capabilities,
+                root_dir = require("lspconfig.util").root_pattern(".terraform", ".git"),
+            }
+            require("lspconfig").gopls.setup {
+                capabilities = capabilities,
+                root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
+                cmd = { "gopls" },
+                settings = {
+                    gopls = {
+                        codelenses = {
+                            generate = true,       -- Activer les code lenses pour la génération de code
+                            gc_details = true,     -- Afficher les détails du garbage collector
+                        },
+                        usePlaceholders = true,    -- Utiliser des placeholders pour les paramètres de fonction
+                        completeUnimported = true, -- Compléter les symboles non importés
+                    },
+                },
+            }
             require("lspconfig").jsonls.setup {
                 cmd = { "vscode-json-languageserver", "--stdio" },
                 filetypes = { "json", "jsonc" },
@@ -45,47 +67,15 @@ return {
 
             require("lspconfig").html.setup {
                 capabilities = capabilities,
+                root_dir = require("lspconfig.util").root_pattern("package.json", ".git"),
                 cmd = { "vscode-html-language-server", "--stdio" },
                 filetypes = { "html" },
-                --[[ init_options = {
-                    configurationSection = { "html", "css", "javascript" },
-                    embeddedLanguages = {
-                        css = true,
-                        javascript = true
-                    }
-                }, ]]
-                --[[ settings = {
-                    html = {
-                        format = {
-                            enable = true
-                        },
-                        validate = {
-                            scripts = true,
-                            styles = true
-                        },
-                        suggest = {
-                            html5 = true
-                        }
-                    }
-                } ]]
             }
             require("lspconfig").stylelint_lsp.setup {
                 capabilities = capabilities,
+                root_dir = require("lspconfig.util").root_pattern("package.json", ".git"),
+                cmd = { "stylelint-lsp", "--stdio" },
                 filetypes = { "css", "scss", "less", "sass", "postcss" }, -- Types de fichiers pris en charge
-                --[[ settings = {
-                    stylelintplus = {
-                        autoFixOnSave = true,                                -- Corrige automatiquement les erreurs lors de l'enregistrement
-                        validateOnSave = true,                               -- Valide les fichiers lors de l'enregistrement
-                        configFile = vim.fn.expand("~/stylelint.config.js"), -- Spécifie un fichier de config
-                    },
-                }, ]]
-                --[[ cmd = { "vscode-css-language-server", "--stdio" },
-                filetypes = { "css", "scss", "less" },
-                settings = {
-                    css = { validate = true },
-                    scss = { validate = true },
-                    less = { validate = true }
-                } ]]
             }
         end,
     }

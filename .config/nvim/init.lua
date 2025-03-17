bddda3e06299a9702dd3dc08c5db21a88184a365
Zeeward41 <zeeward41@gmail.com>
2025-03-17 23:55:20 +0100

    comment: unable_lsp_lint

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,8 @@ require("config.lazy")
 require("config.remap")
 require("config.set")
 require("config.mod.maximize_window")
-require("config.mod.unable_lsp_lint")
+-- require("config.mod.unable_lsp_lint")
+
 
 vim.o.termguicolors = true
 --vim.opt.fileformat = "unix"

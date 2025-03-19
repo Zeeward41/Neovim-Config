c60b1a0c23edfb2e2eb7dc4595bb1dad897847f6
Zeeward41 <zeeward41@gmail.com>
2025-03-19 15:31:05 +0100

    change: colorscheme (new tokyonight)

--- .config/nvim/lua/config/plugins/horizon.lua
+++ .config/nvim/lua/config/plugins/horizon.lua
@@ -1,6 +1,7 @@
 return {
     {
         'akinsho/horizon.nvim',
+        enabled = false,
         config = function()
             vim.cmd.colorscheme('horizon')
             vim.o.background = "dark"
